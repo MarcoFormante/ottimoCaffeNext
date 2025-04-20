a70a7ea0bd3aedda2763d2189804f0735cfcfb1c
Marco <formante.marco@gmail.com>
2025-04-20 23:18:00 +0200

    modify next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "admin.novacreatives.org",  
+        destination: "/admin",  
+      },
+    ];
+  },
+
 };
 
 export default nextConfig;
