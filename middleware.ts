97fa5e83559275bb232f15f1edc73672cdddd7a8
Marco <formante.marco@gmail.com>
2025-04-20 23:28:50 +0200

    test

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,14 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+// import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
-  const url = request.nextUrl;
-  const host = request.headers.get('host');
+// export function middleware(request: NextRequest) {
+//   const url = request.nextUrl;
+//   const host = request.headers.get('host');
 
  
-  if (url.pathname.startsWith('/admin') && host !== 'admin.miosito.com') {
-    return NextResponse.redirect(new URL('/404', request.url)); 
-  }
+//   if (url.pathname.startsWith('/admin') && host !== 'admin.miosito.com') {
+//     return NextResponse.redirect(new URL('/404', request.url)); 
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
